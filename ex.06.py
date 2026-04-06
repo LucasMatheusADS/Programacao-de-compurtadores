379f73679e2820325533228a5de92d3f1385bef0
LucasMatheusADS <lucasbr5300x@gmail.com>
2026-04-06 08:23:20 -0300

    ex.06.py

--- ex.06.py
+++ ex.06.py
@@ -2,9 +2,7 @@ p1 = int(input('Digite primeiro nmr: '))
 p2 = int(input('Digite segundo nmr: '))
 if p1 > p2:
     print('Primeiro numero e maior')
-elif p1 == p2:
-    print('Numeros iguais')
 elif p2 > p1:
     print('Segundo numero e maior')
 else:
-    print('Segundo numero e maior')    print('Numeros iguais')
+    print('Numeros iguais')
