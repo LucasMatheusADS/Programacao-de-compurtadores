1d38fb9e2b4607b302ae202acca25ee441a53da8
LucasMatheusADS <lucasbr5300x@gmail.com>
2026-04-06 07:24:51 -0300

    feature: atividades de Joanacelle

---
+++ ex.021.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número inteiro: '))
+if n > 0:
+    if n % 2 == 0 and n % 3 == 0:
+        print('Múltiplo de 2 e 3')
+    else:
+        print('Não atende')
+else:
+    print('Número inválido')
